0c67f7771cad557cf43fae454664d2fc703b2bc7
michimurri <joselo.okumura@gmail.com>
2024-07-23 20:27:22 -0300

    Main Screen buttons

--- app/src/main/java/com/chimy/valkaapp/ui/main/MainScreen.kt
+++ app/src/main/java/com/chimy/valkaapp/ui/main/MainScreen.kt
@@ -1,12 +1,18 @@
 package com.chimy.valkaapp.ui.main
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,40 +21,116 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.chimy.valkaapp.ui.theme.ValkaAppTheme
 
 
 @Composable
 fun MainScreen(navController: NavHostController = rememberNavController()) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        contentAlignment = Alignment.Center
     ) {
-        Button(onClick = { navController.navigate("contadores") }) {
-            Text(text = "Contadores")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate("reglamento") }) {
-            Text(text = "Reglamento")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate("galeria") }) {
-            Text(text = "Galería")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate("ranking") }) {
-            Text(text = "Ranking")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate("contacto") }) {
-            Text(text = "Contacto")
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            val buttonBorderModifier = Modifier.border(
+                width = 2.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = MaterialTheme.shapes.medium,
+
+                )
+            Button(
+                onClick = { navController.navigate("contadores") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .then(buttonBorderModifier),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary // Fondo usando color del tema
+                )
+            ) {
+                Text(
+                    text = "Contadores",
+                    color = MaterialTheme.colorScheme.onPrimary
+                ) // Texto usando color del tema
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { navController.navigate("reglamento") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .then(buttonBorderModifier),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary // Fondo usando color del tema
+                )
+            ) {
+                Text(
+                    text = "Reglamento",
+                    color = MaterialTheme.colorScheme.onPrimary
+                ) // Texto usando color del tema
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { navController.navigate("galeria") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .then(buttonBorderModifier),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary // Fondo usando color del tema
+                )
+            ) {
+                Text(
+                    text = "Galería",
+                    color = MaterialTheme.colorScheme.onPrimary
+                ) // Texto usando color del tema
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { navController.navigate("ranking") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .then(buttonBorderModifier),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary // Fondo usando color del tema
+                )
+            ) {
+                Text(
+                    text = "Ranking",
+                    color = MaterialTheme.colorScheme.onPrimary
+                ) // Texto usando color del tema
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { navController.navigate("contacto") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .then(buttonBorderModifier),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary // Fondo usando color del tema
+                )
+            ) {
+                Text(
+                    text = "Contacto",
+                    color = MaterialTheme.colorScheme.onPrimary
+                ) // Texto usando color del tema
+            }
         }
     }
 }
-@Preview
+
+@Preview(backgroundColor = 0xFFF8F5F5, showBackground = true)
 @Composable
-fun MainScreenPreview(){
-    MainScreen()
+fun MainScreenPreview() {
+    ValkaAppTheme {
+        MainScreen()
+    }
 }
--- app/src/main/java/com/chimy/valkaapp/ui/theme/Color.kt
+++ app/src/main/java/com/chimy/valkaapp/ui/theme/Color.kt
@@ -2,19 +2,6 @@ package com.chimy.valkaapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val Orange80 = Color(0xFFFFA726) // Light Orange
 val Orange60 = Color(0xFFFF9800) // Medium Orange
 val Orange40 = Color(0xFFFF5722) // Dark Orange
